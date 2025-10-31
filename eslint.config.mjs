6b7ed14ca946a3d603c091c038e4bcf1a21ebedd
luen <Luen@users.noreply.github.com>
2025-10-31 15:52:51 +1000

    chore: update ESLint configuration to include core web vitals and TypeScript support, and change lint command to use ESLint directly

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,17 +1,16 @@
+import nextCoreWebVitals from "eslint-config-next/core-web-vitals";
+import nextTypescript from "eslint-config-next/typescript";
 import nextConfig from 'eslint-config-next';
 
-const eslintConfig = [
-  {
-    ignores: [
-      '.next/**',
-      'node_modules/**',
-      'out/**',
-      '.vercel/**',
-      'dist/**',
-    ],
-  },
-  ...nextConfig,
-];
+const eslintConfig = [...nextCoreWebVitals, ...nextTypescript, {
+  ignores: [
+    '.next/**',
+    'node_modules/**',
+    'out/**',
+    '.vercel/**',
+    'dist/**',
+  ],
+}, ...nextConfig];
 
 export default eslintConfig;
 
